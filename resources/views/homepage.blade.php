896989201bf63031b47ccb0ad0e29bc981d37281
ValerioScano <valerioscano00@gmail.com>
2026-04-23 17:42:52 +0200

    created functionin homepage with datas arriving from view()

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    <h1>Sono io, la Homepage!</h1>
+    <p>Un progetto di {{$name}}</p>
+</body>
+</html>
